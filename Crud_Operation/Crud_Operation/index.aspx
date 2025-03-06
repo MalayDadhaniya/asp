a0246d86bbbe5a19802b26f9e1ecf6f3b257fb9e
MalayDadhaniya <malaydadhaniya21@gmail.com>
2025-03-07 00:55:30 +0530

    07-03-2025

---
+++ Crud_Operation/Crud_Operation/index.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Crud_Operation.index" EnableEventValidation="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Name:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Gender:<asp:RadioButtonList ID="RadioButtonList1" runat="server">
+                <asp:ListItem>Male</asp:ListItem>
+                <asp:ListItem>Female</asp:ListItem>
+            </asp:RadioButtonList>
+            <br />
+            <br />
+            Gmail:<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            City:<asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem>Rajkot</asp:ListItem>
+                <asp:ListItem>Surat</asp:ListItem>
+                <asp:ListItem>Junagudh</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Save" />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+                <Columns>
+                    <asp:TemplateField HeaderText="Delete">
+                        <ItemTemplate>
+                            <asp:Button ID="Button2" runat="server" CommandArgument='<%# Eval("id") %>' OnClick="Button2_Click" Text="Delete" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Select">
+                        <ItemTemplate>
+                            <asp:Button ID="Button3" runat="server" CommandArgument='<%# Eval("id") %>' OnClick="Button3_Click" Text="Select" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Crud_Operation/Crud_Operation/index.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Crud_Operation
+{
+    
+    public partial class index : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@" /* connection */");
+        SqlCommand cmd = new SqlCommand();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Print();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // Insert code
+
+            if(Button1.Text == "save")
+            {
+                cmd = new SqlCommand("/* Insert SQL code */", con);
+
+                //  part of add parameters = cmd.Parameters.AddWithValue();
+
+                con.Open();
+                int res = cmd.ExecuteNonQuery();
+                con.Close();
+                if(res == 1)
+                {
+                    //Clear selection control
+                    TextBox1.Text = "";
+                    RadioButtonList1.ClearSelection();
+                }
+                Print();
+            }
+            if(Button1.Text == "Update")
+            {
+                cmd = new SqlCommand("/* Update SQL code */", con);
+
+                //  part of add parameters = cmd.Parameters.AddWithValue();
+
+                // add only in this
+
+                cmd.Parameters.AddWithValue("id", ViewState["id"]);
+
+                con.Open();
+                int res = cmd.ExecuteNonQuery();
+                con.Close();
+                if (res == 1)
+                {
+                    //Clear selection control
+                    TextBox1.Text = "";
+                    RadioButtonList1.ClearSelection();
+                    Button1.Text = "save";
+                }
+                Print();
+            }
+        }
+
+        public void Print()
+        {
+            SqlDataAdapter adpt = new SqlDataAdapter("/* Select Query */", con);
+            DataTable dt = new DataTable();
+            adpt.Fill(dt);
+            //GridView1.DataSource(dt);
+            GridView1.DataBind();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            cmd = new SqlCommand("/* DElete Qurey  +btn.CommendArgument  */",con);
+            // cmd.Parameters.AddWithValue();
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Print();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            SqlDataAdapter adpt = new SqlDataAdapter("/*Select Query with where +btn.CommendArgument*/ ", con);
+            DataTable dt = new DataTable();
+            adpt.Fill(dt);
+            TextBox1.Text = dt.Rows[0][1].ToString();
+            RadioButtonList1.Text = dt.Rows[0][2].ToString();
+            ViewState["id"] = btn.CommandArgument;
+            Button1.Text = " Update";
+        }
+    }
+}
